50ed65577fa4255c20ceff4b2e02d90d51b6a360
Viczdera <64205557+Viczdera@users.noreply.github.com>
2021-04-20 19:25:18 +0100

    published

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./components/Home";
+import Checkout from "./components/Checkout";
 
 function App() {
   return (
-    <div>
-      {/*Header*/}
-      <Header />
-      {/*Body*/}
-      <Home />
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          {/*Home and firstpage route*/}
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+
+          <Route path="/checkout/">
+            <Checkout />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
